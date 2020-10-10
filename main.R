f69f88b312373897c4bfebfaa4ce4c2cc917f5f8
Alexandre Gouy <contact@agouy.com>
2020-10-10 20:08:21 +0200

    typo

--- main.R
+++ main.R
@@ -9,7 +9,7 @@ if (length(ctx$rnames) != 1) stop("One and only one row factor is required.")
 type <- "all"
 if(!is.null(ctx$op.value('type'))) type <- ctx$op.value('type')
 what <- ' '
-if(!is.null(ctx$op.value('what'))) type <- ctx$op.value('what')
+if(!is.null(ctx$op.value('what'))) what <- ctx$op.value('what')
 by <- ""
 if(!is.null(ctx$op.value('by'))) by <- ctx$op.value('by')
 
